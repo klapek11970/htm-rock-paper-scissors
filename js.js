95926896c980a84e6917a341b04e05be95f3eb0a
Patryk Zahorski <maarcane@gmail.com>
2019-02-02 12:31:08 +0100

    done

--- js.js
+++ js.js
@@ -8,7 +8,9 @@ window.onload = function () {
 
 	var round = 1
 	var roundtarget = 1
-
+	
+	var items = ["rock.svg","paper.svg","scissors.svg"]
+	
 	var DOM = getElements()
 
 	DOM["start-game"].addEventListener('click', function() {
@@ -16,20 +18,22 @@ window.onload = function () {
 	})
 
 	DOM["rock"].addEventListener('click', function() {
-		play(1)
+		play(0)
 	})
 	DOM["paper"].addEventListener('click', function() {
-		play(2)
+		play(1)
 	})
 	DOM["scissors"].addEventListener('click', function() {
-		play(3)
+		play(2)
 	})
 	
 	
 	function game_init(){
 		scoreP1 = 0
 		scoreP2 = 0
-
+		P1select = 1
+		P2select = 1
+		
 		round = 1
 		roundtarget = 1
 		roundtarget = parseInt(window.prompt("How many rounds you wanna play?\nMax:6",1))
@@ -48,22 +52,23 @@ window.onload = function () {
 	
 	function play(a){
 		round++
+		P1select = a
+		P2select = Math.floor(Math.random()*3)
 		
-		P2select = Math.floor(Math.random()*3) + 1
-		
-		if(a==P2select){
+		if(P1select==P2select){
 			
-		}else if(a==2 & P2select==1){
+		}else if(P1select==1 & P2select==0){
 			scoreP1++
-		}else if(a==1 & P2select==3){
+		}else if(P1select==0 & P2select==2){
 			scoreP1++
-		}else if(a==3 & P2select==1){
+		}else if(P1select==2 & P2select==0){
 			scoreP1++
 		}else{
 			scoreP2++
 		}
 		
 		update_stats()
+		
 		if(round>=roundtarget){
 			DOM["round"].innerHTML = roundtarget+"/"+roundtarget
 			if(scoreP1>scoreP2){
@@ -80,16 +85,18 @@ window.onload = function () {
 		
 	}
 	
-
 	function update_stats(){
 		DOM["score"].innerHTML = scoreP1+":"+scoreP2
 		DOM["round"].innerHTML = round+"/"+roundtarget
+		
+		DOM["img1"].src = items[P1select]
+		DOM["img2"].src = items[P2select]
 	}
 	
 	function getElements(){
 		var newarr=[]
 		var key
-		for (key of ["start-game","menu-ingame","rock","paper","scissors","score","round","scoreboard"]){
+		for (key of ["start-game","menu-ingame","rock","paper","scissors","score","round","img1","img2"]){
 			newarr[key] = document.getElementById(key)
 		}
 		return newarr;
